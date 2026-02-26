0307b8e50bfb55c26d5ffe3564918c1a30aef703
dhanunay <in.dhanunjay@gmail.com>
2026-02-26 10:33:34 +0530

    Update axi_coverage.sv

--- axi_coverage.sv
+++ axi_coverage.sv
@@ -1,6 +1,4 @@
 covergroup wr_tx_fields(string my_name, ref axi_wr_tx tx) with function sample( logic[STROBE_WIDTH-1:0] wstrb);
-  
-  
   option.per_instance = 1;
   option.at_least =1;
   option.name = my_name;
@@ -9,28 +7,25 @@ covergroup wr_tx_fields(string my_name, ref axi_wr_tx tx) with function sample( 
   cp_awaddr : coverpoint tx.awaddr{
     bins b1 = {[0 :'hFF]};
     bins b2 = {['h100:'hFF_FF]};
-    
   }
   
-cp_awsize : coverpoint tx.awsize {
-bins b1[$clog2(DATA_WIDTH >> 3)+1] = {[0:$clog2(DATA_WIDTH >> 3)]};  //    0, 1, 2  for 4 bytes Data bus 
-}
-
-cp_awlen : coverpoint tx.awlen {
-  bins b1[] = {[0:10]};
-}
-
-cp_awburst :coverpoint tx.awburst {
-bins b1 = {INCR};
-bins b2 = {WRAP};
-ignore_bins b3 = {FIXED};
-}
+  cp_awsize : coverpoint tx.awsize {
+  bins b1[$clog2(DATA_WIDTH >> 3)+1] = {[0:$clog2(DATA_WIDTH >> 3)]};  //    0, 1, 2  for 4 bytes Data bus 
+  }
+  
+  cp_awlen : coverpoint tx.awlen {
+    bins b1[] = {[0:10]};
+  }
+  
+  cp_awburst :coverpoint tx.awburst {
+  bins b1 = {INCR};
+  bins b2 = {WRAP};
+  ignore_bins b3 = {FIXED};
+  }
 
   cp_wstrb : coverpoint wstrb {
-
     bins b1[$clog2(DATA_WIDTH >> 3)+1] = {[1:(DATA_WIDTH >> 3)]} with ( $onehot(item)) ; // 1 2 4 8 16 .... bytes
 }
-
 endgroup
 
 
